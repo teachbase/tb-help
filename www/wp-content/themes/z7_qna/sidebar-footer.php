4a33d7e2f37688d312c8bbf202a4de46b915d988
Alex Artyukh <0931165517a@gmail.com>
2015-04-23 13:49:52 +0300

    Footer css fix

--- www/wp-content/themes/z7_qna/sidebar-footer.php
+++ www/wp-content/themes/z7_qna/sidebar-footer.php
@@ -5,40 +5,36 @@
 	&& !is_active_sidebar( 'fifth-footer-widget-area') 
 	&& !is_active_sidebar( 'sixth-footer-widget-area')) 
 return;?>
-<div class="sidebar-footer row">
-	<div class="footer-section col-lg-7">
-		<div class="col-md-4 widget-area">
-			<?php if ( is_active_sidebar( 'first-footer-widget-area' ) ) : ?>
-				<?php dynamic_sidebar( 'first-footer-widget-area' ); ?> 
-			<?php endif; ?>
-		</div>
-		<div class="col-md-4 widget-area">
-			<?php if ( is_active_sidebar( 'second-footer-widget-area' ) ) : ?>
-				<?php dynamic_sidebar( 'second-footer-widget-area' ); ?> 
-			<?php endif; ?>
-		</div>
-		<div class="col-md-4 widget-area">
-			<?php if ( is_active_sidebar( 'third-footer-widget-area' ) ) : ?>
-				<h4 class="copyright">Teachbase, 2012—<?php echo date("Y"); ?></h4>
-				<?php dynamic_sidebar( 'third-footer-widget-area' ); ?> 
-			<?php endif; ?>
-		</div>
+<div class="sidebar-footer">
+	<div class="col-md-4 col-lg-2 widget-area">
+		<?php if ( is_active_sidebar( 'first-footer-widget-area' ) ) : ?>
+			<?php dynamic_sidebar( 'first-footer-widget-area' ); ?> 
+		<?php endif; ?>
 	</div>
-	<div class="footer-section col-lg-5">
-		<div class="col-md-3 widget-area">
-			<?php if ( is_active_sidebar( 'fourth-footer-widget-area' ) ) : ?>
-				<?php dynamic_sidebar( 'fourth-footer-widget-area' ); ?> 
-			<?php endif; ?>
+	<div class="col-md-4 col-lg-2 widget-area">
+		<?php if ( is_active_sidebar( 'second-footer-widget-area' ) ) : ?>
+			<?php dynamic_sidebar( 'second-footer-widget-area' ); ?> 
+		<?php endif; ?>
+	</div>
+	<div class="col-md-4 col-lg-2 widget-area">
+		<?php if ( is_active_sidebar( 'third-footer-widget-area' ) ) : ?>
+			<h4 class="copyright">Teachbase, 2012—<?php echo date("Y"); ?></h4>
+			<?php dynamic_sidebar( 'third-footer-widget-area' ); ?> 
+		<?php endif; ?>
+	</div>
+	<div class="col-md-4 col-lg-2 widget-area">
+		<?php if ( is_active_sidebar( 'fourth-footer-widget-area' ) ) : ?>
+			<?php dynamic_sidebar( 'fourth-footer-widget-area' ); ?> 
+		<?php endif; ?>
 		</div>
-		<div class="col-md-4 widget-area">
-			<?php if ( is_active_sidebar( 'fifth-footer-widget-area' ) ) : ?>
-				<?php dynamic_sidebar( 'fifth-footer-widget-area' ); ?> 
-			<?php endif; ?>
-		</div>
-		<div class="col-md-5 widget-area">
-			<?php if ( is_active_sidebar( 'sixth-footer-widget-area' ) ) : ?>
-				<?php dynamic_sidebar( 'sixth-footer-widget-area' ); ?> 
-			<?php endif; ?>
-		</div>
+	<div class="col-md-4 col-lg-2 widget-area">
+		<?php if ( is_active_sidebar( 'fifth-footer-widget-area' ) ) : ?>
+			<?php dynamic_sidebar( 'fifth-footer-widget-area' ); ?> 
+		<?php endif; ?>
+	</div>
+	<div class="col-md-4 col-lg-2 widget-area">
+		<?php if ( is_active_sidebar( 'sixth-footer-widget-area' ) ) : ?>
+			<?php dynamic_sidebar( 'sixth-footer-widget-area' ); ?> 
+		<?php endif; ?>
 	</div>
 </div>
